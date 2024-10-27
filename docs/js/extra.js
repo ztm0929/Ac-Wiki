7f2206366e2900c6b44ffffd7741d23a6c87630a
SkyHighR <skyhighrr@outlook.com>
2024-10-28 05:36:01 +0800

    更新了1些配置： 1.添加页面更新日期作者插件 2.修复新窗口打开链接只在文章中 ...

--- docs/js/extra.js
+++ docs/js/extra.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll('a[href^="http"]');
-    links.forEach(link => {
+    const articleLinks = document.querySelectorAll('.md-content a[href^="http"]');
+    articleLinks.forEach(link => {
         link.setAttribute('target', '_blank');
         link.setAttribute('rel', 'noopener noreferrer');
     });
